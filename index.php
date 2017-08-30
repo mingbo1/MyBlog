461e6c30b8f59264b91a6565a9221c6450564a69
mingbo1 <572957715@qq.com>
2017-08-30 17:20:02 +0800

    简单模拟中奖；

--- index.php
+++ index.php
@@ -8,3 +8,21 @@ if(!empty$name)
 }else{
  echo"整错了";
 }
+$num = rand(1,50);
+$info = "";
+switch ($num){
+	case 1:
+		$info = "一等奖"；
+		break；
+	case 2：
+		$info = "二等奖";
+		break；
+	case 3:
+		$info = "三等奖";
+		break;
+	case 4:
+		$info = "很遗憾，没有中奖";
+	
+}
+
+echo $info ;
